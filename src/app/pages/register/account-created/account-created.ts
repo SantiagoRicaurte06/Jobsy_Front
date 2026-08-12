ccd809a7aef86c4dd5b6bc81ab09e8f17aa83bd7
victor <victoreduardomarinovega@gmail.com>
2026-08-12 17:05:11 -0500

    se hace correcciones dentro de los archivos account-created.html y account-created.ts

--- src/app/pages/register/account-created/account-created.ts
+++ src/app/pages/register/account-created/account-created.ts
@@ -1,8 +1,6 @@
-
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { LogoComponent } from '../../../shared/components/logo/logo';
-
 
 @Component({
   selector: 'jobsy-account-created',
@@ -13,11 +11,8 @@ import { LogoComponent } from '../../../shared/components/logo/logo';
 })
 export class AccountCreatedPage {
   readonly nextSteps = [
-    { icon: '\u{1F4C4}', tittle: 'Completa tu Hoja de vida',path: '/app/hoja-de-vida' },
-    { icon: '\u{1F50D}', tittle: 'Explora empleos disponibles', path: '/empleos' },
-    { icon: '\{2699}', tittle: 'Configura tus preferencias', path: '/app/configuracion' },
+    { icon: '\u{1F4C4}', title: 'Completa tu hoja de vida', path: '/app/hoja-de-vida' },
+    { icon: '\u{1F50D}', title: 'Explora empleos disponibles', path: '/empleos' },
+    { icon: '\u{2699}', title: 'Configura tus preferencias', path: '/app/configuracion' },
   ];
 }
-
-
-
