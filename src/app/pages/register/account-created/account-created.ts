9dc700018c6bad9701c66155f3ce8ffc5782c9fa
victor <victoreduardomarinovega@gmail.com>
2026-08-12 15:39:14 -0500

    Dentro de la carpeta register se creo el archivo account-created.ts junto con su codigo

--- src/app/pages/register/account-created/account-created.ts
+++ src/app/pages/register/account-created/account-created.ts
@@ -1,12 +1,23 @@
+
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { LogoComponent } from '../../../shared/components/logo/logo';
+
 
 @Component({
   selector: 'jobsy-account-created',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, LogoComponent],
   templateUrl: './account-created.html',
   styleUrl: './account-created.scss',
 })
 export class AccountCreatedPage {
-  // TODO: inyectar servicios y cargar datos.
+  readonly nextSteps = [
+    { icon: '\u{1F4C4}', tittle: 'Completa tu Hoja de vida',path: '/app/hoja-de-vida' },
+    { icon: '\u{1F50D}', tittle: 'Explora empleos disponibles', path: '/empleos' },
+    { icon: '\{2699}', tittle: 'Configura tus preferencias', path: '/app/configuracion' },
+  ];
 }
+
+
+
