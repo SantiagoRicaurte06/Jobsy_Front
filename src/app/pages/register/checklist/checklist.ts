286ac284a434bc7191d834a03b9022350f24cf19
victor <victoreduardomarinovega@gmail.com>
2026-08-13 13:03:31 -0500

    dentro de la carpeta register se tiene una carpeta llamada checklist y dentro de ella se crea un archivo llamado checklist.html junto con su codigo

--- src/app/pages/register/checklist/checklist.ts
+++ src/app/pages/register/checklist/checklist.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'jobsy-checklist',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './checklist.html',
   styleUrl: './checklist.scss',
 })
