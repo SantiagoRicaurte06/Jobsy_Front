4cef95ffdd3f21a1daf2783a86a4ea71489dbe02
Juan Esteban Valencia A. <jv2063616@gmail.com>
2026-08-14 16:05:23 -0500

    Implementar el diseño y los estilos de la página «404 Not Found»

--- src/app/pages/not-found/not-found.ts
+++ src/app/pages/not-found/not-found.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { LogoComponent } from '../../shared/components/logo/logo';
 
 @Component({
   selector: 'jobsy-not-found',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, LogoComponent],
   templateUrl: './not-found.html',
   styleUrl: './not-found.scss',
 })
-export class NotFoundPage {
-  // TODO: inyectar servicios y cargar datos.
-}
+export class NotFoundPage {}
